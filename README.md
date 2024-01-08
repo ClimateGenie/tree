0b4adc586fe9ff818593a34f0f95c9bbdd65e3da
Fonzzy1 <alfiechadwick@hotmail.com>
2024-01-08 16:55:48 +1100

    Update README with expanded information on Climate Genie
    Enhanced README to include detailed explanation and future prospects of the Climate Genie project.
    Our latest change expands on the problem Climate Genie solves by addressing climate change misinformation and detailing how it is spread.
    We provide an in-depth look at the proposed web extension solution which aims to combat misinformation by flagging it and guiding users to reputable sources.
    The README now describes who would benefit from using Climate Genie and specifically mentions the tool's reach and ease of use.
    We've added descriptions of current capabilities, such as detecting and debunking common misconceptions, and outlined its dual function as a data collection instrument.
    Looking forward, we described potential improvements to our AI model, research opportunities, reporting possibilities, diverse deployment methods, and the potential for partnerships to broaden the impact.
    Overall, this revision offers a comprehensive overview of Climate Genie's objectives, functionality, and ambitions for growth.

---
+++ README.md
@@ -0,0 +1,67 @@
+# Climate Genie Future Prospects
+
+## What Problem Are we solving
+
+Climate Change misinformation excludes members of the general population from contributing meaningfully to climate change discussions by creating an educational inequity.
+
+This misinformation is commonly spread online, and therefore can reach many individuals rapidly. With the increasing prevalence of AI in our modern world, misinformation is being produced at an unprecedented rate, and traditional methods of fact checking are not able to keep up.
+
+## A web extension as a solution to Climate Change Misinformation
+
+Given the spread of climate change misinformation a solution requires a scalable and adaptable method of neutralising climate change misinformation - as such we pose an automated tool that provides educational resources for climate change misinformation at the same rate as it is spread.
+
+A web extension that can read along with users and flag any potential misinformation, while referring readers back to reputable sources will limit misinformation's ability to embed itself in the readers mind.
+
+## Who would use Climate Genie
+
+Most people get their news from the internet, therefore making the potential install base extremely large. We have intentionally made the barrier to entry  extremely low, and the impact on the browsing experience minimal in most cases. This should allow maximal uptake amongst people who are even the slightest bit concerned about climate change misinformation.
+
+## What Climate Genie can Do
+
+Currently, the Climate Genie extension works on most News Sites and Blogs. It can detect and debunk over 80 common pieces of climate change misinformation.
+
+In parallel to this, the climate genie extension functions as a data gathering tool to see what type of misinformation is being read and where.
+
+## Where we can take it in the Future
+
+After releasing the extension, there are many things we can do to improve it or to branch out to new projects.
+
+### Improving Our Modeling
+
+Although our AI model for detecting climate change misinformation is pretty good right now, we still have room to improve it. The ways we can do this are:
+
+ - Increasing the amount of training data or compute power we have for it to improve the performance
+ - Increasing the number of common climate denialist arguments that it can detect
+ - Remaking the model in different languages to work on a global scale
+
+### Research and Publications
+
+As part of the ongoing development of the extension and it's ability to collect data on it's user's habits, there various possibilities when looking at research projects and reports.
+
+### Academic Work
+
+ - Formal Deconstruction of New Claims
+ - A Recreation of the work in Coan 2021 with the improved model
+ - Investigating behaviour change with the use of the extension
+
+### Report Style Publications
+
+ - See what misinformation is trending, and potentially provide pre-bunking material 
+ - Misinformation produced by each News Outlet
+ - Breakdown of misinformation surrounding a hot topic
+
+### Deployment Methods
+
+Currently we provide debunking content through the medium of a web extension. However there are other methods through which it may be usefull, such as:
+
+ - A chatbot
+ - Integration with social media (Facebook, X, etc. )
+ - App
+ - Website
+
+### Partnerships
+
+
+
+
+![Tree Image](./tree.png)
---
+++ main.py
@@ -0,0 +1,182 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+from networkx.drawing.nx_pydot import graphviz_layout
+from networkx.generators import social
+
+
+def find_index(strings, l):
+    if len(strings[0]) > l:
+        return 0
+    running_sum = 0
+    for i, s in enumerate(strings):
+        running_sum += len(s)
+        if running_sum > l:
+            return (
+                i - 1
+            )  # return the previous index because this is the largest index with a sum less than l
+    return (
+        len(strings) - 1
+    )  # if sum of all strings is less than l, return the last index
+
+
+def format_label(label, return_length=9):
+    if len(label) <= return_length:
+        return label
+    else:
+        return_string = ""
+        words = label.split(" ")
+        while len(words):
+            i = find_index(words, return_length)
+            return_string += " ".join(words[: i + 1]) + "\n"
+            words = words[i + 1 :]
+        return return_string[:-1]
+
+
+class Node:
+    def __init__(self, name, color="red", graph=None, size=300):
+        self.name = name
+        self.color = color
+        self.size = size
+        self.graph = nx.DiGraph() if graph is None else graph
+        self.graph.add_node(self.name, color=self.color, size=self.size)
+
+    def connect(self, node):
+        self.graph.add_edge(self.name, node.name)
+
+    def create_child(self, name, color=None, size=300):
+        if color:
+            child_color = color
+        else:
+            child_color = self.color
+        child = Node(name, child_color, self.graph, size)
+        self.connect(child)
+        return child
+
+    def draw_graph(self):
+        fig = plt.figure(1, figsize=(18, 6), dpi=300)
+        ax = fig.add_subplot(111)
+        ax.axis("off")
+        pos = graphviz_layout(self.graph, prog="dot")
+
+        colors = [
+            color for name, color in nx.get_node_attributes(self.graph, "color").items()
+        ]
+
+        sizes = [
+            size for name, size in nx.get_node_attributes(self.graph, "size").items()
+        ]
+        # nodes
+        nx.draw_networkx_nodes(self.graph, pos, node_color=colors, node_size=sizes)
+
+        # edges
+        nx.draw_networkx_edges(
+            self.graph,
+            pos,
+            edgelist=self.graph.edges(),
+            arrows=True,
+        )
+
+        # Modify node labels to include newline after every 9 characters
+        labels = {node: format_label(node, 9) for node in self.graph.nodes()}
+
+        # labels
+        nx.draw_networkx_labels(self.graph, pos, labels=labels, font_size=2.5)
+
+        plt.savefig("tree.png", format="PNG")
+
+
+def main():
+    pre_release = Node("Pre Relsease", size=600, color="grey")
+
+    pre_release_reqs = []
+    admin = pre_release.create_child("Admin", "green")
+    for req in [
+        "Fact Checker Language Usage",
+        "Signoff from Legal",
+        "Signoff from marketing",
+    ]:
+        child = admin.create_child(req)
+        pre_release_reqs.append(child)
+
+    marketing = pre_release.create_child("Marketing", "cyan")
+    for req in ["Update Socials", "Promo Materials"]:
+        child = marketing.create_child(req)
+        pre_release_reqs.append(child)
+    marketing.create_child("FB adds")
+    marketing.create_child("Blog posts")
+    marketing.create_child("Social Media Campaigns")
+
+    technical = pre_release.create_child("Technical", "magenta")
+    for req in ["Confirm Scalability", "Move off AWS"]:
+        child = technical.create_child(req)
+        pre_release_reqs.append(child)
+
+    release = Node(
+        "Relsease Climate Genie", color="Yellow", graph=pre_release.graph, size=600
+    )
+    for req in pre_release_reqs:
+        req.connect(release)
+
+    model = release.create_child("Model", "red")
+
+    model.create_child("Other languages")
+    ci = model.create_child("Classification Improvements")
+    ci.create_child("Change Base Model")
+    ci.create_child("More Labeling")
+    inf_speed = ci.create_child("Better Inferance Speed")
+    active_cache = inf_speed.create_child("Active Pre Caching of articles")
+
+    taxonomy = model.create_child("Improve Taxonomy")
+
+    taxonomy.create_child("Remove Un-used or Redundant Claims")
+
+    l1 = taxonomy.create_child("New Level 1 Claims")
+
+    l1.create_child("Green Washing Level 1 Claim")
+
+    taxonomy.create_child("New Level 2 Claims")
+    taxonomy.create_child("New Level 3 Claims")
+    taxonomy.create_child("New Level 4 Claims")
+
+    deploy = release.create_child("Deployment Methods", "Lime")
+    deploy.create_child("Website (12ft.io Style)")
+    deploy.create_child("App")
+    deploy.create_child("Chatbot")
+    social_media = deploy.create_child("Social Media Integration")
+    social_media.create_child("Facebook")
+    social_media.create_child("X")
+    social_media.create_child("Instagram")
+    social_media.create_child("Youtube")
+
+    rnp = release.create_child("Research and Publications", "coral")
+    accademic = rnp.create_child("Academic")
+    accademic.create_child("Tracking Habit Change Through User Data")
+    accademic.create_child("Replication of Coan 2021 with new model")
+
+    deconstrution = accademic.create_child("Deconstrution of new claims")
+    taxonomy.connect(deconstrution)
+
+    reporting = rnp.create_child("Report Style")
+    per_outlet = reporting.create_child("Per Outlet Performance Report")
+    trending = reporting.create_child("Trending Misinformation")
+    trending.create_child("Per Country")
+
+    active_cache.connect(trending)
+
+    partnerships = release.create_child("Partnerships", "dodgerblue")
+
+    partnerships.create_child("Stan Grant")
+    partnerships.create_child("Monash Units")
+    partnerships.create_child("Climate Classroom")
+    partnerships.create_child("Jigsaw")
+    partnerships.create_child("NESP")
+    partnerships.create_child("Jigsaw")
+    producers = partnerships.create_child("News Producers")
+
+    per_outlet.connect(producers)
+
+    pre_release.draw_graph()
+
+
+if __name__ == "__main__":
+    main()
